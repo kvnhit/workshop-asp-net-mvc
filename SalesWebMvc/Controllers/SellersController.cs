4db52986d56c7003c11f4d584ca730c639f88aac
Kevein Hitzschky <kevinhitzschky@outlook.com>
2022-11-25 13:57:16 -0300

    Seller details and eager loading

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -56,5 +56,20 @@ namespace SalesWebMvc.Controllers
             _serllerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
+        public IActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _serllerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
     }
 }
